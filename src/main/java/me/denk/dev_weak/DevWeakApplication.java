10c446582e4872109dcec0ed345330f0d1af6bcb
Vinicius Silva <xdenik13@gmail.com>
2024-07-28 21:15:54 -0300

    Create Project

---
+++ src/main/java/me/denk/dev_weak/DevWeakApplication.java
@@ -0,0 +1,13 @@
+ package me.denk.dev_weak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevWeakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevWeakApplication.class, args);
+	}
+
+}
