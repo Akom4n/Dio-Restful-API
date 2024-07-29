beced0b6f34ac6c460e730a1e7a5fa727f4b2181
Vinicius Silva <xdenik13@gmail.com>
2024-07-28 23:08:50 -0300

    Implementação da camada de dominio

---
+++ src/main/java/me/denk/dev_weak/domain/model/News.java
@@ -0,0 +1,8 @@
+package me.denk.dev_weak.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
---
+++ src/main/java/me/denk/dev_weak/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package me.denk.dev_weak.repository;
+
+import me.denk.dev_weak.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
